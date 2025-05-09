f0e6cf0321a4db4af1506734ee42bb0db7b93859
bmsmcgee <98934884+bmsmcgee@users.noreply.github.com>
2025-05-09 16:19:29 -0600

    .

--- Problems/01984. Minimum Difference Between Highest and Lowest of K Scores/C++/Solution.cpp
+++ Problems/01984. Minimum Difference Between Highest and Lowest of K Scores/C++/Solution.cpp
@@ -14,7 +14,7 @@ class Solution {
             }
     
             // Sort Array
-            sort(nums.begin(), nums.end());
+            mergeSort(nums, 0, nums.size() - 1);
     
             cout << "Sorted: ";
             for (int n : nums) {
@@ -44,4 +44,52 @@ class Solution {
             return minDiff;
     
         }
+    
+        void mergeSort(vector<int>& arr, int left, int right) {
+            if (left >= right) {
+                return;
+            }
+    
+            int mid = left + (right - left) / 2;
+    
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+    
+            merge(arr, left, mid, right);
+        }
+    
+        void merge(vector<int>& arr, int left, int mid, int right) {
+            int n1 = mid - left + 1;
+            int n2 = right - mid;
+    
+            vector<int> L(n1);
+            vector<int> R(n2);
+    
+            for (int i = 0; i < n1; ++i) {
+                L[i] = arr[left + i];
+            }
+            for (int j = 0; j < n2; ++j) {
+                R[j] = arr[mid + 1 + j];
+            }
+    
+            int i = 0;
+            int j = 0;
+            int k = left;
+    
+            while(i < n1 && j < n2) {
+                if(L[i] <= R[j]) {
+                    arr[k++] = L[i++];
+                } else {
+                    arr[k++] = R[j++];
+                }
+            }
+    
+            while (i < n1) {
+                arr[k++] = L[i++];
+            }
+            while (j < n2) {
+                arr[k++] = R[j++];
+            }
+        }
+    
     };
