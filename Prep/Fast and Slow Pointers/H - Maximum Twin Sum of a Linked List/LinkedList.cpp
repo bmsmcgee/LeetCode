0b2fd3c8337c6e2a6ec188b313aa8b567082862a
Brandon McGee <bmsmcgee@outlook.com>
2025-07-16 14:23:37 -0600

    Update LinkedList.cpp

--- Prep/Fast and Slow Pointers/H - Maximum Twin Sum of a Linked List/LinkedList.cpp
+++ Prep/Fast and Slow Pointers/H - Maximum Twin Sum of a Linked List/LinkedList.cpp
@@ -5,3 +5,43 @@ LinkedList::LinkedList() {
   head = nullptr;
 }
 
+// Constructor to initialize from a vector of values
+LinkedList::LinkedList(const vector<int>& values) {
+  head = nullptr;
+  createLinkedList(values);
+}
+
+// Function to create  linked list from a vector of values
+void LinkedList::createLinkedList(const vector<int>& values) {
+  if (values.empty()) {
+    head = nullptr;
+    return;
+  }
+
+  head = new ListNode(values[0]);
+  ListNode* current = head;
+  for (size_t i = 1; i < values.size(); i++) {
+    current->next = new ListNode(values[i]);
+    current = current->next;
+  }
+}
+
+// Destructor to free memory
+LinkedList::~LinkedList() {
+  ListNode* current = head;
+  while (current) {
+    ListNode* nextNode = current->next;
+    delete current;
+    current = nextNode;
+  }
+}
+
+// Function to display the linked list
+void display(ListNode* head) {
+  ListNode* current = head;
+  while (current) {
+    cout << current->val << " -> ";
+    current = current->next;
+  }
+  cout << "None" << endl;
+}
